ab73f2169dac676acaf150b8f88377ce90c62cc8
Jay Schlotterback <jawsbak@outlook.com>
2022-07-28 10:52:37 -0400

    tutorial completed

--- chapter07/start_here/routing-global-loader/src/app/app.component.ts
+++ chapter07/start_here/routing-global-loader/src/app/app.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  isLoadingRoute = false;
+
   // DO NOT USE THE CODE BELOW IN PRODUCTION
   // IT WILL CAUSE PERFORMANCE ISSUES
   constructor(private auth: AuthService, private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.isLoadingRoute = true;
+      }
+
+      if (
+        event instanceof NavigationCancel ||
+        event instanceof NavigationEnd ||
+        event instanceof NavigationError
+      ) {
+        this.isLoadingRoute = false;
+      }
+    });
   }
 
   get isLoggedIn() {
