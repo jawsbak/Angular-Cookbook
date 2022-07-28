72a74a31ad0d18d53d07aeb4d6fca06e2bda9186
Jay Schlotterback <jawsbak@outlook.com>
2022-07-28 13:04:03 -0400

    tutorial completed

--- chapter08/start_here/testing-td-forms/src/app/components/release-form/release-form.component.spec.ts
+++ chapter08/start_here/testing-td-forms/src/app/components/release-form/release-form.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { ReleaseFormComponent } from './release-form.component';
+import { ReleaseLog } from 'src/app/classes/release-log';
 
 describe('ReleaseFormComponent', () => {
   let component: ReleaseFormComponent;
@@ -8,9 +9,9 @@ describe('ReleaseFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReleaseFormComponent ]
-    })
-    .compileComponents();
+      declarations: [ReleaseFormComponent],
+      imports: [FormsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +23,58 @@ describe('ReleaseFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should submit a new release log with the correct input values', fakeAsync(async () => {
+    const submitButton = fixture.nativeElement.querySelector(
+      'button[type="submit"]'
+    );
+    const CALENDAR_APP = component.apps[2];
+    spyOn(component.newReleaseLog, 'emit');
+    await fixture.whenStable();
+
+    component.releaseForm.controls['version'].setValue('2.2.2');
+    component.releaseForm.controls['app'].setValue(CALENDAR_APP);
+    submitButton.click();
+
+    const expectedReleaseLog = new ReleaseLog(CALENDAR_APP, '2.2.2');
+    expect(component.newReleaseLog.emit).toHaveBeenCalledWith(
+      expectedReleaseLog
+    );
+  }));
+
+  it("should disable the submit button when we don't have an app selected", fakeAsync(async () => {
+    const submitButton = fixture.nativeElement.querySelector(
+      'button[type="submit"]'
+    );
+    const CALENDAR_APP = component.apps[2];
+    spyOn(component.newReleaseLog, 'emit');
+    await fixture.whenStable();
+
+    const expectedError =
+      'Invalid version provided. Please provide a valid version as (major.minor.patch)';
+    component.releaseForm.controls['version'].setValue('x.x.x');
+    component.releaseForm.controls['app'].setValue(CALENDAR_APP);
+
+    expect(() => component.formSubmit(component.releaseForm)).toThrowError(
+      expectedError
+    );
+    fixture.detectChanges();
+    expect(submitButton.hasAttribute('disabled')).toBe(true);
+    expect(component.newReleaseLog.emit).not.toHaveBeenCalled();
+  }));
+
+  it("should disable the submit button whe we don't have an app selected", fakeAsync(async () => {
+    const submitButton = fixture.nativeElement.querySelector(
+      'button[type="submit"]'
+    );
+    spyOn(component.newReleaseLog, 'emit');
+    await fixture.whenStable();
+
+    component.releaseForm.controls['version'].setValue('2.2.2');
+    component.releaseForm.controls['app'].setValue(null);
+    fixture.detectChanges();
+
+    expect(submitButton.hasAttribute('disabled')).toBe(true);
+    expect(component.newReleaseLog.emit).not.toHaveBeenCalled();
+  }));
 });
