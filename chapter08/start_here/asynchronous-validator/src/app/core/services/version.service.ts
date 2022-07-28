29a3b3c005ebb34bc2d5a7304ebbe56c953e9187
Jay Schlotterback <jawsbak@outlook.com>
2022-07-28 14:28:36 -0400

    tutorial completed

--- chapter08/start_here/asynchronous-validator/src/app/core/services/version.service.ts
+++ chapter08/start_here/asynchronous-validator/src/app/core/services/version.service.ts
@@ -2,19 +2,43 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { delay, map } from 'rxjs/operators';
 import { vLog } from 'src/app/interfaces/vLog';
+import { compareVersion } from 'src/app/utils';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+} from '@angular/forms';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VersionService {
-
   constructor(private http: HttpClient) {}
 
   getVersionLog() {
-    return this.http.get<{vLog: vLog}>('assets/data.json')
-      .pipe(
-        map((res) => res.vLog),
-        delay(2000)
+    return this.http.get<{ vLog: vLog }>('assets/data.json').pipe(
+      map((res) => res.vLog),
+      delay(2000)
+    );
+  }
+
+  versionValidator(appNameControl: AbstractControl): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors> => {
+      if (!appNameControl.value) {
+        return of(null);
+      }
+      return this.getVersionLog().pipe(
+        map((vLog) => {
+          const newVersion = control.value;
+          const previousVersion = vLog[appNameControl.value];
+          return compareVersion(newVersion, previousVersion) === 1
+            ? null
+            : {
+                newVersionRequired: previousVersion,
+              };
+        })
       );
+    };
   }
 }
