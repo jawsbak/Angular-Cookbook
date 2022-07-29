b1e963ebc4c982cf8dd3e065eb3745d64c12c4e1
Jay Schlotterback <jawsbak@outlook.com>
2022-07-29 10:52:19 -0400

    tutorial completed

--- chapter09/start_here/using-cdk-virtual-scroll/src/app/app.module.ts
+++ chapter09/start_here/using-cdk-virtual-scroll/src/app/app.module.ts
@@ -7,20 +7,18 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { TheAmazingListComponent } from './components/the-amazing-list/the-amazing-list.component';
 import { LoaderComponent } from './components/loader/loader.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TheAmazingListComponent,
-    LoaderComponent
-  ],
+  declarations: [AppComponent, TheAmazingListComponent, LoaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    ScrollingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
