2bfc960b58ccfed141505acbb9bb6766c3a727fa
Jay Schlotterback <jawsbak@outlook.com>
2022-07-29 11:16:19 -0400

    tutorial completed

--- chapter09/start_here/using-list-key-manager/src/app/components/the-amazing-list/the-amazing-list.component.ts
+++ chapter09/start_here/using-list-key-manager/src/app/components/the-amazing-list/the-amazing-list.component.ts
@@ -1,16 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  Input,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { AppUserCard } from 'src/interfaces/app-user-card.interface';
+import { FocusKeyManager } from '@angular/cdk/a11y';
+import { TheAmazingListItemComponent } from '../the-amazing-list-item/the-amazing-list-item.component';
 
 @Component({
   selector: 'app-the-amazing-list',
   templateUrl: './the-amazing-list.component.html',
-  styleUrls: ['./the-amazing-list.component.scss']
+  styleUrls: ['./the-amazing-list.component.scss'],
+  host: { role: 'list' },
 })
-export class TheAmazingListComponent implements OnInit {
+export class TheAmazingListComponent implements OnInit, AfterViewInit {
   @Input() listItems: Partial<AppUserCard>[] = [];
-  constructor() { }
 
-  ngOnInit(): void {
+  @ViewChildren(TheAmazingListItemComponent)
+  listItemsElements: QueryList<TheAmazingListItemComponent>;
+
+  private listKeyManager: FocusKeyManager<TheAmazingListItemComponent>;
+
+  @HostListener('window:keydown', ['$event'])
+  onKeyDown(event) {
+    this.listKeyManager.onKeydown(event);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.listKeyManager = new FocusKeyManager(this.listItemsElements);
+  }
 }
