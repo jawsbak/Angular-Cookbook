e672c840e4bd8ab4a346869e16dd583699da5de2
Jay Schlotterback <jawsbak@outlook.com>
2022-08-01 11:31:46 -0400

    tutorial completed

--- chapter10/start_here/mocking-components-with-ng-mocks/src/app/app.component.spec.ts
+++ chapter10/start_here/mocking-components-with-ng-mocks/src/app/app.component.spec.ts
@@ -1,18 +1,18 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { VersionControlComponent } from './components/version-control/version-control.component';
+import { MockComponent } from 'ng-mocks';
 
 const pageTitle = 'Mocking components using ng-mocks';
 
 describe('AppComponent', () => {
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        imports: [RouterTestingModule],
-        declarations: [AppComponent],
-      }).compileComponents();
-    })
-  );
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, MockComponent(VersionControlComponent)],
+    }).compileComponents();
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- chapter10/start_here/mocking-components-with-ng-mocks/src/app/components/version-control/version-control.component.spec.ts
+++ chapter10/start_here/mocking-components-with-ng-mocks/src/app/components/version-control/version-control.component.spec.ts
@@ -1,26 +1,54 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  MockBuilder,
+  MockedComponentFixture,
+  MockRender,
+  ngMocks,
+} from 'ng-mocks';
+import { ReleaseFormComponent } from '../release-form/release-form.component';
+import { ReleaseLogsComponent } from '../release-logs/release-logs.component';
 import { VersionControlComponent } from './version-control.component';
+import { Apps } from 'src/app/constants/apps';
 
 describe('VersionControlComponent', () => {
   let component: VersionControlComponent;
-  let fixture: ComponentFixture<VersionControlComponent>;
-
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [VersionControlComponent],
-      }).compileComponents();
-    })
-  );
+  let fixture: MockedComponentFixture<VersionControlComponent>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(VersionControlComponent);
-    component = fixture.componentInstance;
+    return MockBuilder(VersionControlComponent)
+      .mock(ReleaseFormComponent)
+      .mock(ReleaseLogsComponent);
+  });
+
+  beforeEach(() => {
+    fixture = MockRender(VersionControlComponent);
+    component = fixture.point.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the [logs] @Input for the ReleaseLogsComponent', () => {
+    const releaseLogsComponent =
+      ngMocks.find<ReleaseLogsComponent>('app-release-logs').componentInstance;
+    const logsStub = [{ app: Apps.DRIVE, version: '2.2.2', message: '' }];
+    component.releaseLogs = [...logsStub];
+    fixture.detectChanges();
+    expect(releaseLogsComponent.logs.length).toBe(1);
+    expect(releaseLogsComponent.logs).toEqual([...logsStub]);
+  });
+
+  it('should add the new log when it is created via ReleaseFormComponent', () => {
+    const releaseFormComponent =
+      ngMocks.find<ReleaseFormComponent>('app-release-form').componentInstance;
+    const releaseLogsComponent =
+      ngMocks.find<ReleaseLogsComponent>('app-release-logs').componentInstance;
+    const newLogsStub = { app: Apps.DRIVE, version: '2.2.2', message: '' };
+    component.releaseLogs = [];
+    releaseFormComponent.newReleaseLog.emit(newLogsStub);
+    fixture.detectChanges();
+    expect(component.releaseLogs).toEqual([newLogsStub]);
+    expect(releaseLogsComponent.logs).toEqual([newLogsStub]);
   });
 });
