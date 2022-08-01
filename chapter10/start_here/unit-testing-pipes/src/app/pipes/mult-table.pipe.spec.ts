913d06a0fb3a23287aa401011d876af0a81f4222
Jay Schlotterback <jawsbak@outlook.com>
2022-08-01 12:37:42 -0400

    tutorial completed

--- chapter10/start_here/unit-testing-pipes/src/app/pipes/mult-table.pipe.spec.ts
+++ chapter10/start_here/unit-testing-pipes/src/app/pipes/mult-table.pipe.spec.ts
@@ -9,4 +9,34 @@ describe('MultTablePipe', () => {
   it('create an instance', () => {
     expect(pipe).toBeTruthy();
   });
+
+  it('should return an empty array if the value of digit is not valid', () => {
+    const digit = 0;
+    const limit = 10;
+    const outputArrray = pipe.transform(null, digit, limit);
+    expect(outputArrray).toEqual([]);
+  });
+
+  it('should return an empty array if the value of limit is not valid', () => {
+    const digit = 10;
+    const limit = 0;
+    const outputArray = pipe.transform(null, digit, limit);
+    expect(outputArray).toEqual([]);
+  });
+
+  it('should return the correct multiplication table when both digit and limit inputs are valid', () => {
+    const digit = 10;
+    const limit = 2;
+    const expectedArray = ['10 * 1 = 10', '10 * 2 = 20'];
+    const outputArray = pipe.transform(null, digit, limit);
+    expect(outputArray).toEqual(expectedArray);
+  });
+
+  it('should round off the limit if it is not provided in decimals', () => {
+    const digit = 10;
+    const limit = 3.5;
+    const expectedArray = ['10 * 1 = 10', '10 * 2 = 20', '10 * 3 = 30'];
+    const outputArray = pipe.transform(null, digit, limit);
+    expect(outputArray).toEqual(expectedArray);
+  });
 });
